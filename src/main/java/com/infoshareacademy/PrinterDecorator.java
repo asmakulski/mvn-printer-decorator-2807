527f7ecde29fd61f41fca074ed3e77c6e355686c
Adam Smakulski <adam.smakulski@gmail.com>
2018-07-28 13:06:59 +0200

    first commit

---
+++ src/main/java/com/infoshareacademy/PrinterDecorator.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy;
+
+public class PrinterDecorator {
+
+    public static void decorate(String string){
+        ;
+        Printer.print(string.toUpperCase());
+    }
+}
